5d02e1b60d6a5ec96e48c2369ceccf138b305880
Carlos Marx <carlosmarx27@gmail.com>
2022-10-06 09:49:16 -0300

    Set StateFull

--- lib/home_controller.dart
+++ lib/home_controller.dart
@@ -6,6 +6,10 @@ class HomeController extends InheritedNotifier<ValueNotifier<int>> {
     required Widget child,
   }) : super(key: key, child: child, notifier: ValueNotifier(0));
 
+  static HomeController of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<HomeController>()!;
+  }
+
   int get value => notifier!.value;
 
   increment() {
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,24 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:hello_world/home_controller.dart';
 
-class HomePage extends StatelessWidget {
+// class HomePage extends StatelessWidget {
+//   const HomePage({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     var controller = HomeController.of(context);
+//     final size = MediaQuery.of(context).size;
+//     final navigation = Navigator.of(context);
+//     final theme = Theme.of(context);
+
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Home Page'),
+//       ),
+//       body: Center(
+//         child: Text('Flutterando ${controller.value}'),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () {
+//           controller.increment();
+//         },
+//         child: const Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    var controller =
-        context.dependOnInheritedWidgetOfExactType<HomeController>()!;
+  State<HomePage> createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
+  var counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
       body: Center(
-        child: Text('Flutterando ${controller.value}'),
+        child: Text('Flutterando $counter'),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          controller.increment();
+          setState(() {
+            counter++;
+          });
         },
         child: const Icon(Icons.add),
       ),
